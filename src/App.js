ba620f45529826579f64316ae0fdd448d511eade
basalcode <basalcode@gmail.com>
2021-10-03 22:45:19 +0900

    Fix start error and add test codes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import logo from './logo.svg';
+import { useState } from "react";
 
 function App() {
+  const [item, setItem ] = useState(1);
+  const incrementItem = () => setItem(item + 1);
+  const decrementItem = () => setItem(item - 1);
+  
   return (
     <div className="App">
+      <h1>Hello {item}</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <button onClick={incrementItem}>IncrementItem</button>
+      <button onClick={decrementItem}>DecrementItem</button>
     </div>
   );
 }
