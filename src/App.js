af17d31783148afcf537d116b17194f347909cc4
basalcode <basalcode@gmail.com>
2021-10-06 22:20:31 +0900

    Create basics of useEffect

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
+import { useState, useEffect } from 'react';
 // import { useState } from 'react';
 // import UseState from './UseState';
 // import Input from './Input';
-import Tabs from './Tabs';
+// import Tabs from './Tabs';
 
-const App = () => {
+const App = () => { 
+  const [number, setNumber]  = useState(0);
+  const [aNumber, setAnumber]  = useState(0);
+
+  const sayHello = () => console.log("hello");
+
+  useEffect(sayHello(), [number]);
+
   return (
     <div className="App">
       {/* <UseState /> */}
       {/* <Input /> */}
-      <Tabs />
+      {/* <Tabs /> */}
+      <div>Hi</div>
+      <button onClick={() => setNumber(number + 1)}>{number}</button>
+      <button onClick={() => setAnumber(aNumber + 1)}>{aNumber}</button>
     </div>
   );
 }
