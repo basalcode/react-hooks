18769acba630252ee9a7c5cd62c35524d0608a89
basalcode <basalcode@gmail.com>
2021-10-18 17:45:29 +0900

    Style useNetwork hook example

--- src/views/Network/index.js
+++ src/views/Network/index.js
@@ -1,5 +1,7 @@
 import { useNetwork } from "./useNetwork";
 
+import { network, text, status, status_online, status_offline } from './index.module.scss';
+
 const Network = props => {
     const handleNetworkChange = (online) => {
         console.log(online ? "We just went online" : "We are offline")
@@ -8,7 +10,14 @@ const Network = props => {
     const online = useNetwork(handleNetworkChange);
 
     return (
-        <h1>{online ? "Online" : "Offline"}</h1>
+        <div className={network}>
+            <h1 className={text}>
+                You are in
+            </h1>
+            <h1 className={`${status} ${online ? status_online : status_offline}`}>
+                {online ? "Online" : "Offline"}
+            </h1>
+        </div>
     );
 }
 
