c9dd1dd4226f3cf141d1aae087d76b7ce61c6b9f
basalcode <basalcode@gmail.com>
2021-10-18 17:26:18 +0900

    Style useFadeIn hook example

--- src/views/FadeIn/index.js
+++ src/views/FadeIn/index.js
@@ -1,14 +1,30 @@
 import { useFadeIn } from "./useFadeIn";
 
+import {
+    fade_in,
+    title,
+    text
+} from './index.module.scss';
+
 const FadeIn = props => {
     const fadeInH1 = useFadeIn(1, 1);
     const fadeInP = useFadeIn(2, 2);
 
     return (
-        <>
-            <h1 {...fadeInH1}>Hello</h1>
-            <p {...fadeInP}>Lorem ipsum</p>
-        </>
+        <div className={fade_in}>
+            <h1
+                className={title}
+                {...fadeInH1}
+            >
+                페이드인 효과를
+            </h1>
+            <p
+                className={text}
+                {...fadeInP}
+            >
+                표현해보았다.
+            </p>
+        </div>
     );
 }
 
