87c1beb63c44456d7cc1c422ef063366d31bc9ad
basalcode <basalcode@gmail.com>
2021-10-19 15:51:04 +0900

    Style useScroll hook example

--- src/views/Scroll/index.js
+++ src/views/Scroll/index.js
@@ -1,11 +1,35 @@
 import { useScroll } from "./useScroll";
 
+import { scroll, text_container, title, inside_border, outside_border } from './index.module.scss';
+
 const Scroll = () => {
     const { y } = useScroll();
+    const limitation = 200;
 
     return (
-        <div style={{ height: "1000vh" }}>
-            <h1 style={{ position: "fixed", color: y > 100 ? "red" : "blue" }}>Hi</h1>
+        <div
+            className={scroll}
+        >
+            <div className={text_container}>
+                <h1
+                    className={title}
+                >
+                    스크롤 Y: {y}
+                </h1>
+                <h1
+                    className={title}
+                >
+                    경계선: {limitation}
+                </h1>
+                <h1
+                    className={`${title} ${y > limitation ? inside_border : outside_border}`}
+                >
+                    {y > limitation
+                        ? "초과"
+                        : "정상"
+                    }
+                </h1>
+            </div>
         </div>
     );
 }
--- src/views/Scroll/useScroll.js
+++ src/views/Scroll/useScroll.js
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
 
 export const useScroll = () => {
-    const [state, setState] = useState({
+    const [scrollPosition, setScrollPosition] = useState({
         x: 0,
         y: 0
     });
 
     const onScroll = event => {
-        setState({ x: window.scrollX, y: window.scrollY });
+        setScrollPosition({ x: window.scrollX, y: window.scrollY });
     }
 
     useEffect(() => {
@@ -15,5 +15,5 @@ export const useScroll = () => {
         return () => window.removeEventListener("scroll", onscroll);
     }, []);
 
-    return state;
+    return scrollPosition;
 }
